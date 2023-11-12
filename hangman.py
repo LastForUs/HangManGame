7b2954bff0ad55d9dc659be95bac1c9f22d09251
Omer Cagan <omercagan890@gmail.com>
2023-11-12 14:46:06 +0300

    First Version
    This is our first ever try

---
+++ hangman.py
@@ -0,0 +1,46 @@
+import random
+from words import words
+import string
+
+
+def chose_a_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word
+
+
+def hangman():
+    word = chose_a_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_lowercase)
+    guessed_letters = set()
+
+    lives = 6
+    left_lives = lives
+
+    while len(word_letters) > 0:
+
+        if left_lives < 0:
+            break
+        print(f"You have {lives} more lives")
+        print("You have used these letters", ' '.join(guessed_letters).upper())
+        word_list = [letter if letter in guessed_letters else '-' for letter in word]
+        print('Current Word Is:', ' '.join(word_list).upper())
+        user_letter = input("Chose a letter >>").lower()
+        if user_letter in alphabet - guessed_letters:
+            guessed_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                left_lives -= 1
+
+        elif user_letter in guessed_letters:
+            print("You Have already Guessed it".title())
+        else:
+            print("WTF is that. Guess a word in the alphabet")
+
+
+hangman()
+
+
